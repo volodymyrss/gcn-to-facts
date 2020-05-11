0ccabdb5426319c5a702b0197ec4c1ef719be5b7
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-05-11 14:22:47 +0200

    a test

--- gcnfacts.py
+++ gcnfacts.py
@@ -2,6 +2,7 @@ import re
 import sys
 import requests
 import click
+import rdflib
 from colorama import Fore, Back, Style
 
 workflow_context = []
@@ -24,7 +25,7 @@ def cli():
 def _gcn_source(gcnid: int) -> str:  # -> gcn
     t = requests.get("https://gcn.gsfc.nasa.gov/gcn3/%i.gcn3" % gcnid).text
 
-    print(t)
+    #print(t)
 
     return t
 
@@ -70,17 +71,28 @@ def gcn_countepart_search(gcntext: str):  # ->
 
     original_event = r.groups()[0].strip()
 
-    r = re.search("DATE:(.*?):.*counterpart", gcntext, re.I)
+    original_event_utc = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) UTC, hereafter T0", gcntext).groups()[0]
 
-    return dict(original_event=original_event)
+    return dict(original_event=original_event, original_event_utc=original_event_utc)
+
+@workflow
+def gcn_icecube_circular(gcntext: str):  # ->
+    r = re.search("SUBJECT:(.*?)- IceCube observation of a high-energy neutrino candidate event", 
+                  gcntext, re.I).groups()[0].strip()
+
+    return dict(reports_icecube_event=r)
 
 
 @cli.command()
 @click.argument('gcnid', type=int)
-def gcn_workflows(gcnid):
-    gs = _gcn_source(gcnid)
+@workflow
+def _gcn_workflows(gcnid):
+    gs = gcn_source(gcnid)
 
     print(gs)
+    
+    G = rdflib.Graph()
+    G.bind('gcn', rdflib.Namespace('http://odahub.io/ontology/gcn#'))
 
     for wn, w in workflow_context:
         print("..\n", Fore.BLUE+wn+Style.RESET_ALL, w)
@@ -89,8 +101,20 @@ def gcn_workflows(gcnid):
             o = w(gs)
 
             print(Fore.GREEN+"found:"+Style.RESET_ALL, gcnid, wn, o)
+
+            for k,v in o.items():
+                G.update('INSERT DATA {{ gcn:gcn{gcnid} gcn:{prop} "{value}" }}'.format(gcnid=gcnid, prop=k, value=v))
+
+            print()
+
         except Exception as e:
             print(Fore.YELLOW+"problem"+Style.RESET_ALL, repr(e))
+
+    return G.serialize(format='n3').decode()
+
+@workflow
+def counterpart_sequence():
+    pass
 
 
 if __name__ == "__main__":
--- tests/test_gcn.py
+++ tests/test_gcn.py
@@ -1,4 +1,4 @@
-def test_icecube():
+def test_icecube_counterpart_antares():
     import gcnfacts as gf
 
     mt = gf.gcn_meta(
@@ -6,3 +6,45 @@ def test_icecube():
             )
 
     print(mt)
+
+def test_icecube_counterpart_integral():
+    import gcnfacts as gf
+
+    gcnid = 27652
+
+    mt = gf.gcn_meta(
+                gf.gcn_source(gcnid)
+            )
+
+    print(mt)
+    
+    ct = gf.gcn_countepart_search(
+                gf.gcn_source(gcnid)
+            )
+
+    print(ct)
+
+    #icecube itself
+    
+    ic = gf.gcn_icecube_circular(
+                gf.gcn_source(27651)
+            )
+
+    print(ic)
+    
+def test_workflows():
+    import gcnfacts as gf
+    import rdflib
+
+    G = rdflib.Graph()
+
+    t = gf.gcn_workflows(27652) # integral
+
+    G.parse(data=t, format="n3")
+
+    t = gf.gcn_workflows(27651) # icecube
+
+    G.parse(data=t, format="n3")
+
+    print(G.serialize(format='n3').decode())
+
